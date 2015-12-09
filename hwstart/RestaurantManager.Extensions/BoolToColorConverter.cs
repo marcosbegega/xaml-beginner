d87e3d3fc386dc9311e9834e6efbea2afcebcd20
marcosbegega <marcosbegega@gmail.com>
2015-12-09 23:18:39 +0100

    Behavior implemented

---
+++ hwstart/RestaurantManager.Extensions/BoolToColorConverter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace RestaurantManager.Extensions
+{
+    public class BoolToColorConverter : DependencyObject, IValueConverter
+    {
+        public string TrueColor  { get; set; }
+        public string FalseColor { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool isExpedited = (bool)value;
+
+            var property = typeof(Colors).GetRuntimeProperty("Transparent");
+
+            try
+            {
+                if (isExpedited)
+                {
+                    property = typeof(Colors).GetRuntimeProperty(this.TrueColor);
+                }
+                else
+                {
+                    property = typeof(Colors).GetRuntimeProperty(this.FalseColor);
+                }
+
+                return property.GetValue(null);
+            }
+            catch (NullReferenceException)
+            {
+                property = typeof(Colors).GetRuntimeProperty("Transparent");
+
+                return property.GetValue(null);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            bool returnValue = false;
+
+            try
+            {
+                var trueProperty = typeof(Colors).GetRuntimeProperty(this.TrueColor).GetValue(null);
+                if ((Color)value == (Color)trueProperty)
+                {
+                    returnValue = true;
+                }
+            }
+            catch (NullReferenceException)
+            { }
+
+            return returnValue;
+        }
+    }
+}
---
+++ hwstart/RestaurantManager.Extensions/RightClickMessageDialogBehavior.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Microsoft.Xaml.Interactivity;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Popups;
+
+namespace RestaurantManager.Extensions
+{
+    public class RightClickMessageDialogBehavior : DependencyObject, IBehavior
+    {
+        public string Message { get; set; }
+        public string Title { get; set; }
+
+        public DependencyObject AssociatedObject
+        { get; private set; }
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            if (associatedObject is Page)
+            {
+                this.AssociatedObject = associatedObject;
+                (this.AssociatedObject as Page).RightTapped += RightClickMessageDialogBehavior_RightTapped;
+            }
+        }
+
+        private async void RightClickMessageDialogBehavior_RightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+            await new MessageDialog(this.Message, this.Title).ShowAsync();
+        }
+
+        public void Detach()
+        {
+            this.AssociatedObject = null;
+            (this.AssociatedObject as Page).RightTapped -= RightClickMessageDialogBehavior_RightTapped;
+        }
+    }
+}
