60ae23f6bf95d295e0b7e9ac711c61df569631e0
marcosbegega <marcosbegega@gmail.com>
2015-12-13 22:03:37 +0100

    Eliminate inheritance from DependencyObject

--- hwstart/RestaurantManager.Extensions/BoolToColorConverter.cs
+++ hwstart/RestaurantManager.Extensions/BoolToColorConverter.cs
@@ -10,7 +10,7 @@ using Windows.UI.Xaml.Data;
 
 namespace RestaurantManager.Extensions
 {
-    public class BoolToColorConverter : DependencyObject, IValueConverter
+    public class BoolToColorConverter : IValueConverter
     {
         public Color TrueColor  { get; set; }
         public Color FalseColor { get; set; }
